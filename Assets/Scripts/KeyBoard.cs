bf295d0fb8a75184121ec4d21e8235ff3e820f03
Pariterre <benjamin.michaud.phq@gmail.com>
2024-05-06 16:43:12 -0400

    Added a keyboard so the user can write the ip address in VR

---
+++ Assets/Scripts/KeyBoard.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyBoard : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _inputField;
+    [SerializeField] List<Button> _keyboardKeys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _keyboardKeys.Count; i++)
+        {
+            var button = _keyboardKeys[i];
+            var key = button.GetComponentInChildren<TMP_Text>().text;
+            button.onClick.AddListener(() => OnKeyPressed(key));
+        }
+    }
+
+    void OnKeyPressed(string key)
+    {
+        if (key == "<")
+        {
+            _inputField.text = "";
+        }
+        else
+        {
+            _inputField.text += key;
+        }
+    }
+}
